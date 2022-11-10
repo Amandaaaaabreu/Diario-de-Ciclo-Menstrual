dfbecc0251f3b2dc05bc185e09b84c06f463c395
Amanda <amandaabreu045@gmail.com>
2022-11-10 19:59:30 -0300

    Novos Codigos

---
+++ variaveis.java
@@ -0,0 +1,12 @@
+package Java;
+public class variaveis {
+    
+    //  %d= string %d= inteiro %f= float
+    public static void main(String[] args){
+        int num=13;
+        String nome= "Amanda";
+
+        System.out.printf(" %nValor da variavel num: %d%nNome: %s", num,nome);
+    
+    }
+}
