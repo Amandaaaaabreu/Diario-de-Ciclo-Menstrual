3701c2926b4f4bcdb9fe9850d38533d1c24ec13b
Amanda <amandaabreu045@gmail.com>
2022-12-01 15:36:30 -0300

    Atualizações

--- metodo2.java
+++ metodo2.java
@@ -1,13 +1,15 @@
 public class metodo2{
   
-  public static void main(Strings[] args){
-      msg("Testando 1");
-      msg("Testando 2");
-      msg("Testando 3");
+  public static void main(String[] args){
+      msg("Testando esse modelo",7);
+      
   }
   
-  public static void msg(String m){ 
-      System.out.println("m");
+  public static void msg(String m, int l){
+    for(int i=0;i<l;i++){
+        System.out.println(m);
+    }
+      
   }
   
   
