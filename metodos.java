a5186de9390c843af7684c3dfb6bb059e63cb4c3
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-12-01 14:59:52 -0300

    Update metodos.java

--- metodos.java
+++ metodos.java
@@ -3,7 +3,7 @@ public class metodos{
   // método é uma função que programamos para uma classe
  // o método so vai ser executado quando ele for chamado
 
-            public static void main (Strings[] args) {
+            public static void main (String[] args) {
                   for(int i=0;i<5;i++){ 
                   teste();
             }
