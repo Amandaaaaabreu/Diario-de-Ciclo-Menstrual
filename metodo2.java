a1cb698d797d4407b68ada450e75eb61cc787e5f
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-12-01 14:24:16 -0300

    Create metodo2.java

---
+++ metodo2.java
@@ -0,0 +1,18 @@
+public classe metodo2{
+  
+  public classe void main(Strings[] args){
+      msg("Testando 1");
+      msg("Testando 2");
+      msg("Testando 3");
+  }
+  
+  public static void msg(String m){ 
+      System.out.println("m");
+  }
+  
+  
+  
+  
+  
+  
+  }
