0b4b5e9dc56435304a9d0db164e8f33cfa5fdbbf
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-12-01 13:31:18 -0300

    Create metodos.java

---
+++ metodos.java
@@ -0,0 +1,16 @@
+public class metodos.java{ 
+  
+  // método é uma função que programamos para uma classe
+ // o método so vai ser executado quando ele for chamado
+
+            public static void main (Strings[] args) {
+              teste();
+            }
+            public static void teste(){
+                    System.out.println(" Isso é um teste!");
+            }
+  
+  
+  
+  
+  }
