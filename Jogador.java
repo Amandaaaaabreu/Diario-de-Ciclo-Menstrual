e83cf4105bb8549b2cbc97e91ee03b28d4b24de1
Amanda <amandaabreu045@gmail.com>
2022-12-02 15:28:14 -0300

    att

---
+++ Jogador.java
@@ -0,0 +1,16 @@
+// construtor é um metodo
+// construtor não tem retorno, portanto não precisando especificar o seu tipo 
+// construtor precisa obrigatoriamente ter o mesmo nome da classe
+//
+  public class Jogador {
+ 
+     public Jogador(int n){
+        System.out.printf("%nEstrela numero %d verde%n",n);
+
+     }
+        
+
+
+
+
+ }
