7707e18e29297eb2065ca91631258f1b58ee4984
Amanda <amandaabreu045@gmail.com>
2022-12-04 15:53:00 -0300

    att

--- gets_sets.java
+++ gets_sets.java
@@ -1,5 +1,5 @@
-// o metodo do tipo get servem para obter valores de variaveis de uma classe
+// o metodo do tipo get serve para obter valores de variaveis de uma classe
 
-// o metodo do tipo set servem para atribuir valores a variaveis de uma classe do tipo private
+// o metodo do tipo set serve para atribuir valores a variaveis de uma classe do tipo private
 
 // metodo do tipo set não retorna NADA, logo o retorno é do tipo void.
