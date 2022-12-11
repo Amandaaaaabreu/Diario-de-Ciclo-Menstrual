ba6aada7368651791db69b4a6806b2b7669f3a70
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-12-11 20:29:23 -0300

    Update and rename Principal.java to Main.java

---
+++ trabalho final/Main.java
@@ -0,0 +1,67 @@
+import Fluxo_Menstrual;
+import Ciclo;
+import Aviso;
+import Sintomas;
+import Consulta;
+import Suporte;
+
+
+public class Main {
+    static Usuaria u1;
+    static Fluxo_Menstrual fm;
+    static Ciclo cl;
+    static Suporte sp;
+    static Consulta ct;
+    static Aviso av;
+    static Sintomas st;
+
+public static void main(String[] args){
+        menu();
+    }
+    public void menu(){
+            Scanner scan = new Scanner (System.in);
+            int escolha = 0; 
+            System.out.println("Diário de ciclos menstruais - Menu:\n");
+            System.out.println("Criar Fluxo Menstrual:" + "Digite 1"+ "\n");
+            System.out.println("Verificar estimativa de dias restantes:" + "Digite 2"+ "\n");
+            System.out.println("Ligar lembrete de avisos:" + "Digite 3"+ "\n");
+            System.out.println("Verificar Sintomas:" + "Digite 4"+ "\n");
+            System.out.println("Obter Prontuário:" + "Digite 5"+ "\n");
+            System.out.println("Marcar Consulta:" + "Digite 6"+ "\n");
+            System.out.println("Sair:" + "Digite 7" + "\n");
+            escolha = scan.nextInt();
+                if(escolha == 1){
+                    this.fm = Fluxo_Menstrual.Criar_Fluxo_Menstrual();
+                    menu();
+                }
+                else if(escolha == 2){
+                    System.out.println("Dias Restantes: " + Ciclo.estim_dias_restantes());
+                    menu();
+                }
+                else if(escolha == 3){
+                    Aviso.tomar_anticoncepcional();
+                    menu();
+                }
+                else if(escolha == 4){
+                    this.st = Sintomas.retornar_sintomas();
+                    menu();
+                }
+                else if(escolha == 5){
+                    System.out.println(Consulta.obter_prontuario());
+                    menu();
+                }
+                else if(escolha == 6){
+                    if(Suporte.obter_prontuario()){
+                        System.out.println("Consulta Marcada");
+                    }
+                    else{
+                        System.out.println("Consulta Cancelada");
+                    }
+                    menu();
+                }
+                else{
+                    System.out.println("Valor Inválido\n");
+                    menu();
+                }
+    }
+}
