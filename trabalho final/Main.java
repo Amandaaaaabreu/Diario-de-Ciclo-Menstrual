f641362fc9c27a05fcd81e1ec2c27861a356b6ef
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-12-11 21:38:04 -0300

    Update Main.java

--- trabalho final/Main.java
+++ trabalho final/Main.java
@@ -1,4 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
 import java.util.Scanner;
+import main.Consulta;
 
 public class Main {
     static Usuaria u1;
