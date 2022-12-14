de3402ef184bea7fd051cdeae6e12ef01b9fa609
Amandaaaaabreu <211030925@aluno.unb.br>
2022-12-14 12:28:46 -0300

    att

--- trabalho final/Ciclo.java
+++ trabalho final/Ciclo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+//package main;
 // importando biblioteca
 // import java.sql.Date;
 import java.util.Scanner;
--- trabalho final/Main.java
+++ trabalho final/Main.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+//package main;
 
 import java.util.Scanner;
-import main.Consulta;
 
 public class Main {
     static Usuaria u1;
@@ -23,37 +22,38 @@ public static void main(String[] args){
   public static void menu(){
             Scanner scan = new Scanner (System.in);
             int escolha = 0; 
-            System.out.println("Diário de ciclos menstruais - Menu:\n");
-            System.out.println("Criar Fluxo Menstrual:" + "Digite 1"+ "\n");
-            System.out.println("Verificar estimativa de dias restantes:" + "Digite 2"+ "\n");
-            System.out.println("Ligar lembrete de avisos:" + "Digite 3"+ "\n");
-            System.out.println("Verificar Sintomas:" + "Digite 4"+ "\n");
-            System.out.println("Obter Prontuário:" + "Digite 5"+ "\n");
-            System.out.println("Marcar Consulta:" + "Digite 6"+ "\n");
-            System.out.println("Sair:" + "Digite 7" + "\n");
+            System.out.println("\n-----Diário de ciclos menstruais-----\n");
+            System.out.println("           Menu abaixo:\n");
+            System.out.println("- Criar Fluxo Menstrual:" + " Digite 1"+ "\n");
+            System.out.println("- Verificar estimativa de dias restantes:" + " Digite 2"+ "\n");
+            System.out.println("- Ligar lembrete de avisos:" + " Digite 3"+ "\n");
+            System.out.println("- Verificar Sintomas:" + " Digite 4"+ "\n");
+            System.out.println("- Obter Prontuário:" + " Digite 5"+ "\n");
+            System.out.println("- Marcar Consulta:" + " Digite 6"+ "\n");
+            System.out.println("- Sair:" + " Digite 7" + "\n");
             escolha = scan.nextInt();
                 if(escolha == 1){
-                    fm.Criar_Fluxo_Menstrual();
+                    Fluxo_Menstrual.Criar_Fluxo_Menstrual();
                     menu();
                 }
                 else if(escolha == 2){
-                    System.out.println("Dias Restantes: " + cl.estim_dias_restantes());
+                    System.out.println("Dias Restantes: " + Ciclo.estim_dias_restantes());
                     menu();
                 }
                 else if(escolha == 3){
-                    av.tomar_anticoncepcional();
+                    Aviso.tomar_anticoncepcional();
                     menu();
                 }
                 else if(escolha == 4){
-                    st.retornar_sintomas();
+                    Sintomas.retornar_sintomas();
                     menu();
                 }
                 else if(escolha == 5){
-                    System.out.println(ct.obter_prontuario());
+                    System.out.println(Consulta.obter_prontuario());
                     menu();
                 }
                 else if(escolha == 6){
-                    if(sp.confirmar_consulta()){
+                    if(Suporte.confirmar_consulta()){
                         System.out.println("Consulta Marcada");
                     }
                     else{
