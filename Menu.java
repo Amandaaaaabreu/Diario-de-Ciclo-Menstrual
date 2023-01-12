b32be5b7b1f82850e934ec37951c1d79498b2a74
Amandaaaaabreu <211030925@aluno.unb.br>
2023-01-12 10:24:04 -0300

    att

---
+++ Menu.java
@@ -0,0 +1,17 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Menu {
+
+    public JFrame tela;   // criando a janela principal
+    JLabel titulo = new JLabel("Diário de Ciclo Menstrual");
+
+    JButton Usuaria = new JButton("Alterar Usuária");
+
+    JButton Ciclo = new JButton("Cadastrar meu Ciclo");
+
+    JButton Suporte = new JButton("Marcar Consulta");
+
+    JButton Sintomas = new JButton("Responder questionário");
+}
